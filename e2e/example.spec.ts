e7cd6536bdcce965ac2104ecd07696553ce589dc
Cygnux X-1 <62252129+Dekatron322@users.noreply.github.com>
2025-08-27 23:59:10 +0100

    test: update page title assertion to match new dashboard name

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -3,5 +3,5 @@ import { expect, test } from "@playwright/test"
 test("has title", async ({ page }) => {
   await page.goto("./")
 
-  await expect(page).toHaveTitle(/UltraDash/)
+  await expect(page).toHaveTitle(/Ultra Admin Dashboard/)
 })
